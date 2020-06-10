1505bcd806d05ae6ae1b9a0dd0cc5ed3e4a19490
Qudus Adeyemo <qudus-mac@Quduss-MacBook-Pro.local>
2020-06-10 21:54:11 +0100

    Factory Pattern Added

---
+++ creational/Factory/CouponTypeFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+
+class CouponTypeFactory
+{
+    const MONETARY_COUPON = 'monetary';
+    const GIFT_COUPON = 'gift';
+    const SHIPPING_COUPON = 'shipping';
+
+
+    public function getCouponType(string $type): CouponType {
+        if ($type == self::MONETARY_COUPON)
+            return new MonetaryCoupon();
+
+        if ($type == self::GIFT_COUPON)
+            return new GiftCoupon();
+
+        if ($type == self::SHIPPING_COUPON)
+            return new ShippingCoupon();
+
+        return null;
+
+    }
+}
