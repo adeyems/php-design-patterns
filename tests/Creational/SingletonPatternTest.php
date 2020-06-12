fa5bebb3bbe6f0f548680a0ac05279a3480800f1
ADEYEMO Qudus <adeyems@gmail.com>
2020-06-12 12:55:55 +0000

    Apply fixes from StyleCI

--- tests/Creational/SingletonPatternTest.php
+++ tests/Creational/SingletonPatternTest.php
@@ -4,16 +4,14 @@ declare(strict_types=1);
 
 namespace DesignPatterns\Tests\Creational;
 
-use PHPUnit\Exception;
-use PHPUnit\Framework\TestCase;
 use DesignPatterns\Creational\Singleton\DB;
+use PHPUnit\Framework\TestCase;
 
 /**
  * Class FactoryPatternTest.
  */
 class SingletonPatternTest extends TestCase
 {
-
     public function test_it_can_return_DB_instance()
     {
         $this->assertSame('single connection', DB::getInstance()->getConnection());
